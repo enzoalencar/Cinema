8d9f90e110171f0049cf5706bc43d1213bde59a4
enzoalencar <101678365+enzoalencar@users.noreply.github.com>
2022-04-28 11:02:31 -0300

    Update Identificacao.c

--- Identificacao.c
+++ Identificacao.c
@@ -88,3 +88,68 @@ do
 
 return 0;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Bibliotecas
+#include <stdio.h>//Blioteca Padrão Entrada e Saida
+#include <windows.h>//Biblioteca Funções do Terminal Windows
+#include <conio.h>//Biblioteca para Manipular Caracteres na Tela
+#include <ctype.h>//Biblioteca para Classificar Caracteres
+#include <string.h>
+
+//Cores
+#define RST  "\x1B[0m"//Cor Resetar
+#define RED  "\e[31;1m"//Cor Vermelho
+#define GRN "\e[32;1m"//Cor Verde
+#define YEL "\e[33;1m"//Cor Amarelo
+#define BLU  "\x1B[34m"//Cor Azul
+#define MAG  "\x1B[35m"//Cor Magenta
+#define CYN  "\x1B[36m"//Cor Cyano
+#define WHT  "\e[38;1m"//Cor Branca
+#define REDB  "\e[31;7m"//Vermelho Back Ground
+#define REDN  "\e[31;1m"// negrito
+//Função Principal
+int main()
+{
+	int tamN = 100, tami = 15;
+    int i = 0, vali, cont = 0;
+    char nome[tamN], idade[tami];
+    char letra;
+    int comprimento;
+
+    
+	system("color 00");//Setar Padrão Cor do Terminal
+
+    printf(RED"Nome Completo:"RST);
+
+    do
+    {
+		nome[i] = '\0';
+        letra = getch();
+        if(!isalpha(letra) || letra == 32)
+        {
+        	nome[i] = letra;
+        	i++;
+			printf("%c", letra);
+		}
+		
+		else if(letra == 8 && i)//se for pressionada a tecla BACKSPACE e houver caracteres já digitados
+       {
+          nome[i]='\0';//o índice atual do vetor de caracteres recebe a terminação da string
+          i--;//o índice do vetor de caracteres é decrementado
+          printf("\b \b");//o caractere digitado é apagado da tela
+       }
+       
+	comprimento = strlen(nome);
+	
+	}while(letra != 13 || comprimento < 1);
+	
+	
+	printf("\n%d\n", comprimento);
+
+	printf("\n");
+		
+			printf("%s", nome);
+		
+}
